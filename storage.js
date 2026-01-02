56a7764ae228b855d31ecf96b6d2f4912173b6e1
raffaelEcarella <docwindtrecat@gmail.com>
2026-01-02 20:12:51 +0100

    Delete storage.js

--- storage.js
+++
@@ -1,8 +0,0 @@
-function saveState() {
-  localStorage.setItem("cassa2026", JSON.stringify(appState));
-}
-
-function loadState() {
-  const data = localStorage.getItem("cassa2026");
-  if (data) Object.assign(appState, JSON.parse(data));
-}
