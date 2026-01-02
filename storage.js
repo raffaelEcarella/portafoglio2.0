1864a0975d031ea87bd17d8cb40d255c9a5fac8f
raffaelEcarella <docwindtrecat@gmail.com>
2026-01-02 20:39:31 +0100

    Update storage.js

--- storage.js
+++ storage.js
@@ -1,8 +1,11 @@
-function saveState() {
-  localStorage.setItem("cassa2026", JSON.stringify(appState));
+function saveState(){
+  localStorage.setItem("portafoglio2_0", JSON.stringify(appState));
 }
 
-function loadState() {
-  const data = localStorage.getItem("cassa2026");
-  if (data) Object.assign(appState, JSON.parse(data));
+function loadState(){
+  const data = localStorage.getItem("portafoglio2_0");
+  if(data){
+    const parsed = JSON.parse(data);
+    Object.assign(appState, parsed);
+  }
 }
