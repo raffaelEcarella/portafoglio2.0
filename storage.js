5e5477b086654f8ab3e97ac587341c2aa806fa17
raffaelEcarella <docwindtrecat@gmail.com>
2026-01-02 19:54:58 +0100

    Update storage.js

--- storage.js
+++ storage.js
@@ -6,20 +6,3 @@ function loadState() {
   const data = localStorage.getItem("cassa2026");
   if (data) Object.assign(appState, JSON.parse(data));
 }
-
-function resetAll() {
-  // Rimuove dal localStorage
-  localStorage.removeItem("cassa2026");
-
-  // Reset stato in memoria
-  appState.security = { pin: null, attempts: 0, authenticated: false };
-  appState.ui = { currentPage: "home", darkMode: false };
-  appState.finance = { movimenti: [] };
-
-  // Reset UI
-  document.body.classList.remove("dark");
-  showLogin();
-  showPage("home");
-
-  saveState();
-}
