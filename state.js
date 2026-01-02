9d129211ae82144bfdf33bf916035923f5d1e4cb
raffaelEcarella <docwindtrecat@gmail.com>
2026-01-02 20:45:44 +0100

    Update state.js

--- state.js
+++ state.js
@@ -1,8 +1,7 @@
 const appState = {
   finance: {
     saldo: 0,
-    movimenti: [], // {descrizione, importo, tipo, categoria, data, ricorrenza}
-    ricorrenti: [] // movimenti ricorrenti estesi
+    movimenti: []
   },
   ui: {
     darkMode: false
