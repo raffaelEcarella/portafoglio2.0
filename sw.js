d277c84dc3596063ec67bef7b927d738b6fa1d0e
raffaelEcarella <docwindtrecat@gmail.com>
2026-01-03 20:36:51 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,50 @@
-self.addEventListener("fetch", event=>{
-  event.respondWith(fetch(event.request).catch(()=>caches.match(event.request)));
+// CC99 - sw.js Portafoglio 2.0
+const CACHE_NAME = "portafoglio2_cache_v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./app.html",
+  "./style.css",
+  "./app.js",
+  "./ui.js",
+  "./state.js",
+  "./storage.js",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(name => {
+          if(name !== CACHE_NAME) return caches.delete(name);
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        // copia della risposta in cache
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
 });
