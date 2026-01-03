757d10ddf309b637b37c856ab59e8d2419478215
raffaelEcarella <docwindtrecat@gmail.com>
2026-01-03 17:07:51 +0100

    Update state.js

--- state.js
+++ state.js
@@ -1,16 +1,9 @@
 const appState = {
   finance: {
     saldo: 0,
-    movimenti: [],
-    traguardo: 1000
+    movimenti: []
   },
   ui: {
-    darkMode: false,
-    chartColors: {
-      entrate: "#28a745",
-      spese: "#dc3545",
-      traguardo: "#ffc107",
-      saldo: "#007bff"
-    }
+    darkMode: false
   }
 };
