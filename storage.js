5953d7a8038a4d43602474d2e0d56f24d554f41a
raffaelEcarella <docwindtrecat@gmail.com>
2026-01-03 19:12:23 +0100

    Update storage.js

--- storage.js
+++ storage.js
@@ -1,7 +1,4 @@
-// CC99 - storage.js
-function saveState() { localStorage.setItem("portafoglio2_state", JSON.stringify(appState)); }
-function loadState() {
-  const data = localStorage.getItem("portafoglio2_state");
-  if(data) Object.assign(appState, JSON.parse(data));
-  if(appState.ui.darkMode) document.body.classList.add("dark");
-}
+// CC99 - storage.js v0.95
+// Solo wrappers per compatibilità
+function saveStateWrapper(){ saveState(); }
+function loadStateWrapper(){ loadState(); }
