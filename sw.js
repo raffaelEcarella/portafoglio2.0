a74fdf1fb56b6b7571e303b521492b841c9374c1
raffaelEcarella <docwindtrecat@gmail.com>
2026-01-04 16:12:29 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,5 @@
-const CACHE="p2-cache";
 self.addEventListener("install",e=>{
   e.waitUntil(
-    caches.open(CACHE).then(c=>c.addAll(["./","./index.html"]))
+    caches.open("p2").then(c=>c.addAll(["./","./app.html"]))
   );
 });
