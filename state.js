8ed33504cc890d12c77e91349d8d296fa416aeb7
raffaelEcarella <docwindtrecat@gmail.com>
2026-01-04 18:52:14 +0100

    Update state.js

--- state.js
+++ state.js
@@ -1,13 +1,13 @@
+// CC99 - state.js
 const appState = {
-  ui:{
-    darkMode:false,
-    chartColors:{
-      entrate:"#22c55e",
-      spese:"#ef4444",
-      saldo:"#3b82f6"
-    }
-  },
-  finance:{
-    wallets:[]
-  }
+  finance:{wallets:[]},
+  darkMode:false
 };
+
+function loadState(){
+  const s = localStorage.getItem("appState");
+  if(s) Object.assign(appState, JSON.parse(s));
+}
+function saveState(){
+  localStorage.setItem("appState",JSON.stringify(appState));
+}
