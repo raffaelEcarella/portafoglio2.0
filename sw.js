06f07c6454bce03601414e636cf63555af132f79
raffaelEcarella <docwindtrecat@gmail.com>
2026-01-04 20:14:06 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,16 @@
-self.addEventListener("install",e=>{
-  e.waitUntil(
-    caches.open("p2").then(c=>c.addAll(["./","./app.html"]))
-  );
+const CACHE_NAME = "portafoglio2_cache_v1";
+const urlsToCache = ["./","./index.html","./style.css","./app.js","./ui.js","./state.js","./storage.js","./chart.js"];
+
+self.addEventListener("install", e=>{
+  e.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):null))));
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e=>{
+  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
 });
