bce3c67e73483f9313422bfdd95cc4748452f517
raffaelEcarella <docwindtrecat@gmail.com>
2026-01-04 20:40:56 +0100

    Update state.js

--- state.js
+++ state.js
@@ -1,4 +1,10 @@
-const appState = {
-    movimenti: [],
-    darkMode: false
+// Stato globale dell'app
+const state = {
+  movimenti: [],
+  darkMode: false,
+  getSaldo() {
+    return this.movimenti.reduce((acc, m) => {
+      return m.tipo === 'entrata' ? acc + m.importo : acc - m.importo;
+    }, 0);
+  }
 };
