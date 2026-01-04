0ab4a271a6f565f10715712fa814ee48531f5db3
raffaelEcarella <docwindtrecat@gmail.com>
2026-01-04 18:59:30 +0100

    Update state.js

--- state.js
+++ state.js
@@ -1,13 +1,22 @@
-// CC99 - state.js
-const appState = {
-  finance:{wallets:[]},
-  darkMode:false
-};
+// state.js
 
-function loadState(){
-  const s = localStorage.getItem("appState");
-  if(s) Object.assign(appState, JSON.parse(s));
-}
-function saveState(){
-  localStorage.setItem("appState",JSON.stringify(appState));
-}
+const state = {
+    wallets: [
+        { id: "wallet1", name: "Conto Corrente" },
+        { id: "wallet2", name: "Carta di Credito" }
+    ],
+    movements: JSON.parse(localStorage.getItem("movements")) || [],
+    filters: { wallet: "all", category: "all" },
+
+    addMovement(movement) {
+        this.movements.push(movement);
+        storage.saveMovements(this.movements);
+    },
+
+    getFilteredMovements() {
+        return this.movements.filter(m => 
+            (this.filters.wallet === "all" || m.walletId === this.filters.wallet) &&
+            (this.filters.category === "all" || m.category === this.filters.category)
+        );
+    }
+};
