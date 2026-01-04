fc662711cac39f2229b7520e2a80319fa8cb4793
raffaelEcarella <docwindtrecat@gmail.com>
2026-01-04 20:22:57 +0100

    Update storage.js

--- storage.js
+++ storage.js
@@ -1,9 +1,24 @@
-function saveState() {
-  localStorage.setItem("portafoglio2_state", JSON.stringify(appState));
+function salvaMovimenti() {
+    localStorage.setItem("movimenti", JSON.stringify(appState.movimenti));
 }
 
-function loadState() {
-  const data = localStorage.getItem("portafoglio2_state");
-  if(data) Object.assign(appState, JSON.parse(data));
-  if(appState.ui.darkMode) document.body.classList.add("dark");
+function caricaMovimenti() {
+    const mov = JSON.parse(localStorage.getItem("movimenti") || "[]");
+    appState.movimenti = mov;
 }
+
+function salvaSettings() {
+    localStorage.setItem("darkMode", JSON.stringify(appState.darkMode));
+}
+
+function caricaSettings() {
+    const dm = JSON.parse(localStorage.getItem("darkMode") || "false");
+    appState.darkMode = dm;
+    document.body.classList.toggle("darkmode", dm);
+}
+
+// Carica tutto all'avvio
+document.addEventListener("DOMContentLoaded", () => {
+    caricaMovimenti();
+    caricaSettings();
+});
