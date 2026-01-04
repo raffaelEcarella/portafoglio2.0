84392bb55c3524f9b6879cf326428e703551f487
raffaelEcarella <docwindtrecat@gmail.com>
2026-01-04 20:42:09 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,21 @@
-const CACHE_NAME = "portafoglio2_cache_v1";
-const urlsToCache = ["./","./index.html","./style.css","./app.js","./ui.js","./state.js","./storage.js","./chart.js"];
+const CACHE_NAME = 'finanze-2026-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/movements.html',
+  '/charts.html',
+  '/settings.html',
+  '/style.css',
+  '/app.js',
+  '/state.js',
+  '/storage.js',
+  '/chart.js'
+];
 
-self.addEventListener("install", e=>{
-  e.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache)));
-  self.skipWaiting();
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
-self.addEventListener("activate", e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):null))));
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", e=>{
-  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
