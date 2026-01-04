cbef2a3a0cd29fcf30617547d9c8f89d1d6589ee
raffaelEcarella <docwindtrecat@gmail.com>
2026-01-04 16:10:48 +0100

    Update storage.js

--- storage.js
+++ storage.js
@@ -1,14 +1,9 @@
-// CC99 - storage.js
 function saveState(){
-  localStorage.setItem("portafoglio2_state", JSON.stringify(appState));
+  localStorage.setItem("p2_state", JSON.stringify(appState));
 }
 
 function loadState(){
-  const data = localStorage.getItem("portafoglio2_state");
-  if(data){
-    Object.assign(appState, JSON.parse(data));
-  }
-  if(appState.ui.darkMode){
-    document.body.classList.add("dark");
-  }
+  const d = localStorage.getItem("p2_state");
+  if(d) Object.assign(appState, JSON.parse(d));
+  if(appState.ui.darkMode) document.body.classList.add("dark");
 }
